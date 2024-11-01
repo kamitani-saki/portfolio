5e84a64f3be5a66e6f17a4a5b5fe4b0d2176a2d3
kamitani-saki <sakisaki368@yahoo.co.jp>
2024-11-01 10:25:17 +0900

    11/1

---
+++ js/script.js
@@ -0,0 +1,102 @@
+ //===== sp版のぼたん
+ $(function () {
+    // sp_btnがクリックされたとき
+    $('.sp_btn').on('click', function () {
+        // .sp_btnを.openに切り替える
+        $('.sp_btn').toggleClass('open');
+        // 消えている.sp_navを表示させる
+        $('.sp_header_menu').fadeToggle();
+        $('.sp_logo').fadeToggle();
+    })
+})
+const changeVisibility = () => {
+    var ele = $('.pc_logo');
+
+    if (ele.css('visibility') == 'hidden') {
+        ele.css('visibility', 'visible');
+    } else {
+        ele.css('visibility', 'hidden');
+    }
+}
+//======SVGアニメーションの描画
+var stroke;
+stroke = new Vivus('mask', {//アニメーションをするIDの指定
+    start: 'manual',//自動再生をせずスタートをマニュアルに
+    type: 'scenario-sync',// アニメーションのタイプを設定
+    duration: 1000,//アニメーションの時間設定。数字が小さくなるほど速い
+    forceRender: false,//パスが更新された場合に再レンダリングさせない
+    animTimingFunction: Vivus.EASE,//動きの加速減速設定
+}
+);
+$(window).on('load', function () {
+    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
+    $("#splash_logo").delay(1500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
+    stroke.play();//SVGアニメーションの実行
+});
+
+//======== キャッチコピーの浮き出る文字
+function BlurTextAnimeControl() {
+    $('.slideConts').each(function () {
+        var elemPos = $(this).offset().top - 50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass('slide');
+        } else {
+            $(this).removeClass('slide');
+        }
+    });
+}
+$(window).on('load', function () {
+    BlurTextAnimeControl();
+});
+
+// ======スクロールしたら下からふわっと
+$(window).scroll(function () {
+    $('.fadein').each(function () {
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+
+        if (scroll > elemPos - windowHeight + 150) {
+            $(this).addClass('scrollin');
+        }
+    });
+});
+
+//===== worksスライダー
+$(function () {
+    $('.works_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        dots: false,
+        arrows: false
+    });
+});
+
+// ========スクロールしたら現れるtoppageボタン
+//pagetop
+$(function () {
+    var topBtn = $('.pagetop');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            //ボタンの表示方法
+            topBtn.fadeIn();
+        } else {
+            //ボタンの非表示方法
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,  html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
