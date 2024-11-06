a373491a35e230914134468f8bac93abd45872ff
kamitani-saki <sakisaki368@yahoo.co.jp>
2024-11-06 12:40:56 +0900

    11/6 1

--- js/script.js
+++ js/script.js
@@ -24,14 +24,14 @@ var stroke;
 stroke = new Vivus('mask', {//アニメーションをするIDの指定
     start: 'manual',//自動再生をせずスタートをマニュアルに
     type: 'scenario-sync',// アニメーションのタイプを設定
-    duration: 300,//アニメーションの時間設定。数字が小さくなるほど速い
+    duration:500,//アニメーションの時間設定。数字が小さくなるほど速い
     forceRender: false,//パスが更新された場合に再レンダリングさせない
     animTimingFunction: Vivus.EASE,//動きの加速減速設定
 }
 );
 $(window).on('load', function () {
-    $("#splash").delay(500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
-    $("#splash_logo").delay(500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
+    $("#splash").delay(6000).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
+    $("#splash_logo").delay(6000).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
     stroke.play();//SVGアニメーションの実行
 });
 
@@ -70,6 +70,7 @@ const mySwiper = new Swiper('.swiper', {
     slidesPerView: 'auto',
     spaceBetween: 20,
     grabCursor: true,
+    mousewheel: true,
     pagination: {
       el: '.swiper-pagination',
       type: "progressbar",
@@ -78,7 +79,7 @@ const mySwiper = new Swiper('.swiper', {
 // ========スクロールしたら現れるtoppageボタン
 //pagetop
 $(function () {
-    var topBtn = $('.pagetop');
+    var topBtn = $('.page_top');
     topBtn.hide();
     //スクロールが100に達したらボタン表示
     $(window).scroll(function () {
